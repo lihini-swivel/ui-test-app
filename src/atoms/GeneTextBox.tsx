70d29e0bc66498ae54bc83d33988812338a178ea
lihini-swivel <lihini@swivelgroup.com.au>
2023-05-04 11:28:37 +0530

    Initial commit

---
+++ src/atoms/GeneTextBox.tsx
@@ -0,0 +1,12 @@
+import TextField from '@mui/material/TextField'
+
+const GeneTextBox = () => {
+  return (
+    <>
+      {' '}
+      <TextField id='outlined-basic' label='Outlined' variant='outlined' />
+    </>
+  )
+}
+
+export default GeneTextBox
